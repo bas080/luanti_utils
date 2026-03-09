2fa3a1da557419c64350e783518e90c6579fede8
bas080 <bassimhuis@gmail.com>
2026-03-09 17:44:14 +0200

    Add documentation to extend group and item utils

--- extend_group.lua
+++ extend_group.lua
@@ -1,3 +1,24 @@
+--- Extend callbacks for all nodes in a specific group.
+--
+-- Iterates over `core.registered_items` and applies `extend_item` to
+-- every node belonging to the given group.
+--
+-- @module extend_group
+
+--- Extend all nodes in a group.
+--
+-- @tparam string group_name Name of the group to target
+-- @tparam table extend_def Table of callbacks to extend (see `extend_item`)
+--
+-- @usage
+-- extend_group("leaves", {
+--   after_destruct = function(next, pos, old_node)
+--     minetest.remove_node(pos)
+--     if next then next(pos, old_node) end
+--   end
+-- })
+--
+-- @function extend_group
 local extend_item = luanti_utils.dofile("extend_item.lua")
 
 local extend_group = function(group_name, extend_def)
--- extend_item.lua
+++ extend_item.lua
@@ -1,4 +1,31 @@
--- Only supports callbacks on the def for now.
+--- Extend callbacks of a registered node by wrapping them.
+-- Allows injecting behavior before/after an existing callback while still
+-- optionally calling the original implementation via `next`.
+--
+-- The provided callback receives `next` as its first argument. Calling `next(...)`
+-- will invoke the original callback if it exists.
+--
+-- @module extend_item
+
+--- Extend callbacks on a node definition.
+-- Internally uses `core.override_item` to replace callbacks with wrappers.
+--
+-- @tparam string node_name Name of the node in `core.registered_nodes`
+-- @tparam table extend_def Table of callbacks to extend
+-- @tparam function extend_def.<callback> Replacement callback that receives
+-- `next` as its first parameter followed by the original callback arguments.
+--
+-- @usage
+-- extend_item("default:stone", {
+--   on_punch = function(next, pos, node, puncher, pointed_thing)
+--     minetest.chat_send_all("Stone punched!")
+--     if next then
+--       return next(pos, node, puncher, pointed_thing)
+--     end
+--   end
+-- })
+--
+-- @function extend_item
 local extend_item = function(node_name, extend_def)
   -- Get the original node definition
   local node_def = core.registered_nodes[node_name]
