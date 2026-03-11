75aa7a80fd36429931aa42c2230818a405938477
bas080 <bassimhuis@gmail.com>
2026-03-11 10:12:11 +0200

    Add migrate_node helper

--- migrate_node.lua
+++ migrate_node.lua
@@ -1,5 +1,10 @@
-function migrate_node(lbm_definition)
-  core.register_lbm(lbm_definition)
+function migrate_node(name, nodenames, action)
+  core.register_lbm({
+    name = name,
+    run_at_every_load = true, -- TODO: Should only run during dev.
+    nodenames = nodenames,
+    action = action,
+  })
 end
 
 return migrate_node
