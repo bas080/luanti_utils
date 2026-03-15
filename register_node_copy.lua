436624bd868f6794b85c2c94c133fca9b892d71c
bas080 <bassimhuis@gmail.com>
2026-03-15 12:08:41 +0200

    Document register_node_copy better

--- register_node_copy.lua
+++ register_node_copy.lua
@@ -1,14 +1,12 @@
---- Helper to register nodes with extra features
+--- Re-use existing node definitions and build ontop of them.
+-- The return value is merged into a copy of the node definition before registering.
 -- @module register_node_copy
-
-local table_merge = luanti_utils.dofile('table_merge.lua')
-
---- Register a node like `core.register_node` but with additional features.
--- Prevents mutation of the original definition and supports `copy_of`.
 --
--- @tparam string node_name The name of the node to register.
--- @tparam table def Table containing node properties, may include `copy_of`.
--- @raise Error if `copy_of` references a non-existent node.
+-- @tparam string node_name Name of the new node.
+-- @tparam string copied_node_name The name of the node to copy.
+-- @tparam function def_fn Callback with arg being the node def that is being copied.
+-- @treturn nil
+-- 
 -- @usage
 -- local register_node_copy = require("register_node")
 -- register_node("mymod:leaf", "copy:node", function(copy) return {
@@ -16,6 +14,9 @@ local table_merge = luanti_utils.dofile('table_merge.lua')
 --     tiles = {"leaf.png"},
 --     copy_of = "default:leaves"
 -- })
+
+local table_merge = luanti_utils.dofile('table_merge.lua')
+
 local function register_node_copy(node_name, copied_node_name, def_fn)
     -- Make a deep copy to prevent external mutation
     local parent = table.copy(core.registered_items[copied_node_name])
