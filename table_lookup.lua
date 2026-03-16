fdd51e9cdcbc54910edee2cf131ec817bb69141e
bas080 <bassimhuis@gmail.com>
2026-03-16 14:03:45 +0200

    Add wallmounted_to_facedir lookup util

---
+++ table_lookup.lua
@@ -0,0 +1,49 @@
+--- Get values by key or value.
+-- @module table_lookup.lua
+--
+-- @usage
+-- local by_key, by_value = table_lookup({
+--   "red" = "wool:red",
+--   "blue" = "wool:blue",
+-- })
+--
+-- by_key("red") -- "wool:red"
+-- by_value("wool:blue") -- "blue"
+-- @raise When a value has multiple keys.
+--
+-- @tparam table tbl
+--
+-- @treturn function by_key
+-- @treturn function by_value
+local function table_lookup(tbl)
+  local reverse = {}
+
+  for k, v in pairs(tbl) do
+    if reverse[v] ~= nil then
+      error(("duplicate value '%s' for keys '%s' and '%s'")
+        :format(v, reverse[v], k))
+    end
+
+    reverse[v] = k
+  end
+
+  --- Lookup value by key
+  -- @function by_key
+  -- @tparam any key
+  -- @treturn any
+  local function by_key(key)
+    return tbl[key]
+  end
+
+  --- Lookup key by value
+  -- @function by_value
+  -- @tparam any value
+  -- @treturn any
+  local function by_value(value)
+    return reverse[value]
+  end
+
+  return by_key, by_value
+end
+
+return table_lookup
---
+++ wallmounted_to_facedir.lua
@@ -0,0 +1,32 @@
+--- Convert wallmounted to facedir and back param2
+-- 
+-- @module wallmounted_to_facedir.lua
+--
+-- @usage
+-- local to_facedir, to_wallmounted = luanti_utils('wallmounted_to_facedir.lua')
+-- to_facedir(default_sign.param2) -- Calculate the facedir equivalent for wallmounted sign.
+-- to_wallmounted(vine_param2) -- Calculate wallmounted equivalent of facedir vine.
+
+
+local table_lookup = luanti_utils.dofile('table_lookup.lua')
+
+---
+-- @function to_facedir
+-- @tparam integer wallmounted
+-- @treturn integer facedir
+
+---
+-- @function to_wallmounted
+-- @tparam integer facedir
+-- @treturn integer wallmounted
+
+local wallmounted_to_facedir = {
+	[2] = 19,
+	[4] = 10,
+	[3] = 13,
+	[5] = 4,
+	[1] = 2,
+	[0] = 22,
+}
+
+return table_lookup(wallmounted_to_facedir)
