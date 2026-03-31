7d4d456c6215043c2d5524a2920f374eae093a12
bas080 <bassimhuis@gmail.com>
2026-03-31 23:06:03 +0300

    Prevent restarting round_robin when calling start

--- round_robin.lua
+++ round_robin.lua
@@ -9,72 +9,76 @@
 -- @tparam function on_item
 -- @treturn RoundRobin
 local function round_robin(items_fn, on_item)
-    assert(type(items_fn) == "function", "items_fn must be a function")
-    assert(type(on_item) == "function", "on_item must be a function")
+	assert(type(items_fn) == "function", "items_fn must be a function")
+	assert(type(on_item) == "function", "on_item must be a function")
 
-    local self = {}
-    local items = {}
-    local prev_items = {}
-    local index = 1
-    local paused = true
+	local self = {}
+	local items = {}
+	local prev_items = {}
+	local index = 1
+	local paused = true
 
-    local function process_next()
-        if paused then return end
+	local function process_next()
+		if paused then
+			return
+		end
 
-        print('paused ' .. dump(paused))
-        if index > #items then
-            prev_items = items
-            items = items_fn(prev_items) or {}
-            index = 1
-            if #items == 0 then
-                paused = true
-                return
-            end
-        end
+		if index > #items then
+			prev_items = items
+			items = items_fn(prev_items) or {}
+			index = 1
+			if #items == 0 then
+				paused = true
+				return
+			end
+		end
 
-        local item = items[index]
-        index = index + 1
+		local item = items[index]
+		index = index + 1
 
-        local done_called = false
-        local function done()
-            if done_called then return end
-            done_called = true
-            process_next()
-        end
+		local done_called = false
+		local function done()
+			if done_called then
+				return
+			end
+			done_called = true
+			process_next()
+		end
 
-        on_item(done, item)
-    end
+		on_item(done, item, items)
+	end
 
-    function self:start()
-        print('called')
-        paused = false
-        items = items_fn()
-        
-        print('keep going')
-        process_next()
-    end
+	function self:start()
+		if not paused then
+			return
+		end
+		paused = false
+		items = items_fn()
 
-    function self:resume()
-        self:start()
-    end
+		process_next()
+	end
 
-    function self:restart()
-        paused = false
-        index = 1
-        prev_items = {}
-        items = items_fn(prev_items) or {}
-        if #items == 0 then
-            paused = true
-            return
-        end
-        process_next()
-    end    
+	function self:resume()
+		self:start()
+	end
 
-    function self:pause()
-        paused = true
-    end
+	function self:restart()
+		paused = false
+		index = 1
+		prev_items = {}
+		items = items_fn(prev_items) or {}
+		if #items == 0 then
+			paused = true
+			return
+		end
+		process_next()
+	end
 
-    return self
+	function self:pause()
+		paused = true
+	end
+
+	return self
 end
 
 return round_robin
