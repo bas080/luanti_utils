fa26a3b549335d5df69eafa5aac1d154a841ea6d
bas080 <bassimhuis@gmail.com>
2026-03-31 18:47:18 +0300

    Add random_each function to loop over random items
    Does not pick same item twice.

---
+++ random_each.lua
@@ -0,0 +1,29 @@
+--- More efficient loops over random set of items.
+-- Will NOT contain duplicate picks.
+--
+-- @module random_each.lua
+--
+-- @tparam table items List of items to randonly pick from.
+-- @tparam function on_item Called for each randomly picked item.
+-- @tparam[opt] number ratio The ratio of items to pick.
+--
+-- @usage
+-- local items = {1,2,3}
+--
+-- -- Randomly pick half of the items in the table rounded down.
+-- random_each(items, on_item, 0.5)
+
+local table_shallow_copy = luanti_utils.dofile("table_shallow_copy.lua")
+
+local function random_each(items, on_item, ratio)
+	items = table_shallow_copy(items)
+	local n = math.min(math.floor(ratio * #items), #items)
+
+	for i = 1, n do
+		local j = math.random(i, #items)
+		items[i], items[j] = items[j], items[i] -- swap
+		on_item(items[i]) -- yield this item
+	end
+end
+
+return random_each
